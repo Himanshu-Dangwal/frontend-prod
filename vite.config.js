a3f3a5b1475b503f2ae7f9375b9d1ed5e8395a02
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-10-03 19:38:30 -0700

    a

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow connections from outside the container
+    host: true, // Allow connections from outside the container
     port: 5173, // Optional, this is the default port for Vite
   },
 })
