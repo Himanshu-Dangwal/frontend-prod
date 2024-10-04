c4d9eae334dbe16daac9261a60739f1eebf596a0
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-10-03 19:25:35 -0700

    Updated Dockerfile and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow connections from outside the container
+    port: 5173, // Optional, this is the default port for Vite
+  },
 })
